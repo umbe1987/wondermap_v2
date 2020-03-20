10b730fe3b4eaca1dff1216e7fe01a91bb78dc29
Firstname Lastname <umbertofilippo@tiscali.it>
2020-03-20 11:09:34 +0100

    operational and base layers added to map

--- src/Map.ts
+++ src/Map.ts
@@ -2,22 +2,45 @@ import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
+import { OperationalLayer, BasemapLayer } from './Layer';
 
 export class WonderMap {
     private olMap: Map;
 
-    constructor() {
+    constructor(id: string) {
         this.olMap = new Map({
-            target: 'map',
-            layers: [
-              new TileLayer({
-                source: new OSM()
-              })
-            ],
+            target: document.getElementById(id),
             view: new View({
               center: [0, 0],
               zoom: 0
             })
         })
+        this.addWonderBasemap(new BasemapLayer("OSM"));
+        this.addWonderLayer(new OperationalLayer(
+          "piste da sci",
+          "https://www.wondermap.it/cgi-bin/qgis_mapserv.fcgi?map=/home/umberto/qgis/projects/Demo_sci_WMS/demo_sci.qgs&",
+          "piste_sci"
+        ))
     }
+
+    private addWonderLayer(layer: OperationalLayer): void {
+        this.olMap.addLayer(new ImageLayer({
+          title: layer.title,
+          source: new ImageWMS({
+              ratio: 1,
+              params: {'LAYERS': layer.layerName},
+              url: layer.url
+          })
+      }));
+    }
+
+    private addWonderBasemap(basemap: BasemapLayer): void {
+      this.olMap.addLayer(new TileLayer({
+        title: basemap.title,
+        type: basemap.type,
+        source: new OSM()
+      }));
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { WonderMap } from './Map';
 
-new WonderMap();
+const map = new WonderMap('map');
