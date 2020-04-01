d566309f16c49f4e3619f290ec786af6a5d8e393
Firstname Lastname <umbertofilippo@tiscali.it>
2020-04-01 15:15:32 +0200

    css styles reorganization for widget panel

--- src/widgets/toc/toc.ts
+++ src/widgets/toc/toc.ts
@@ -3,9 +3,9 @@ import { Widget } from '../Widget';
 export class ToC extends Widget {
     
     constructor() {
-        super();
+        super("TABLE OF CONTENTS");
 
-        this.element.classList.add("toc-panel");
+        this.element.classList.add("toc-widget");
         // static folder must exists in the dist folder and html files must be copied to it!
         let filePath = './static/toc.template.html';
         let selector = '#toc';
