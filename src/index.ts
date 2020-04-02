b96f1e6b6db8c42a8e605284d20183c416c2d18e
Firstname Lastname <umbertofilippo@tiscali.it>
2020-04-03 00:37:07 +0200

    widget definition moved to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import 'ol/ol.css';
 import '../style/wondermap.css'
 import { WonderMap } from './Map';
+import { ToC } from './widgets/toc/toc';
 
+// define widgets
+const widgets = [new ToC()];
+
+// define layers
+// TODO: pass layers to the map and remove their definition from Map.ts
+
+// define map
 const map = new WonderMap('map');
+map.addWonderWidgets(widgets);
