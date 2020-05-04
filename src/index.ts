4421df652281723c76fae4a2773c58804f24ab82
Firstname Lastname <umbertofilippo@tiscali.it>
2020-05-04 21:30:12 +0200

    added useful comments to index

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,20 @@ import Collection from 'ol/Collection';
 import { WidgetBar } from './widgets/widgetBar';
 import { ToC } from './widgets/toc/toc';
 
-// define layers
+// define basemaps
 const basemaps = new Collection([new BasemapLayer()]);
 
-// define map
+// define map and add basemaps to it
 const map = new WonderMap('map', basemaps);
 
+// define WMS URLs
 const layerURLs = ["https://www.wondermap.it/cgi-bin/qgis_mapserv.fcgi?map=/home/umberto/qgis/projects/Demo_sci_WMS/demo_sci.qgs&"];
 
 // define widgets and add them to the widget-bar
 const widgets = [
+                // define ToC and which layers will be inserted
                 new ToC(map, layerURLs)
             ];
 
+// add widgets to the widget bar
 new WidgetBar(map, widgets);
