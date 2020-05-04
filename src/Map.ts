c6498cd8a67cd4e2265e70347f8bc661d6f7fdb1
Firstname Lastname <umbertofilippo@tiscali.it>
2020-05-04 15:46:58 +0200

    use of Collection instead of arrays

--- src/Map.ts
+++ src/Map.ts
@@ -1,12 +1,14 @@
 import {Map, View} from 'ol';
-import BaseLayer from 'ol/layer/Base';
+import { OperationalLayer } from './layers/OperationalLayer';
+import { BasemapLayer } from './layers/BasemapLayer';
 import Control from 'ol/control/Control';
+import Collection from 'ol/Collection';
 
 export class WonderMap {
   private wonderMap: Map;
 
   // https://stackoverflow.com/a/43326461/1979665
-  constructor(id: string, layers: BaseLayer[] = []) {
+  constructor(id: string, layers: Collection<OperationalLayer | BasemapLayer>) {
     this.wonderMap = new Map({
       target: document.getElementById(id),
       view: new View({
@@ -19,10 +21,14 @@ export class WonderMap {
     this.addLayers(layers);
   }
 
-  addLayers(layers: BaseLayer[]) {
+  addLayers(layers: Collection<OperationalLayer | BasemapLayer>) {
     layers.forEach(lyr => {
       this.wonderMap.addLayer(lyr);
     })
+  }
+
+  getLayers(): Collection<OperationalLayer | BasemapLayer> {
+    return this.wonderMap.getLayers() as Collection<OperationalLayer | BasemapLayer>;
   }
 
   addControl(control: Control) {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import 'ol/ol.css';
 import '../style/wondermap.css'
 import { WonderMap } from './Map';
-import { WonderLayer } from './layers/OperationalLayer';
+import { OperationalLayer } from './layers/OperationalLayer';
 import { BasemapLayer } from './layers/BasemapLayer';
+import Collection from 'ol/Collection';
 import { WidgetBar } from './widgets/widgetBar';
 import { ToC } from './widgets/toc/toc';
 
@@ -11,16 +12,16 @@ const basemaps = [
     new BasemapLayer(),
 ]
 const operationalLayers = [
-    new WonderLayer(
+    new OperationalLayer(
     "https://www.wondermap.it/cgi-bin/qgis_mapserv.fcgi?map=/home/umberto/qgis/projects/Demo_sci_WMS/demo_sci.qgs&",
     "piste_sci"
     ),
 ];
-const layers = [...basemaps, ...operationalLayers];
+const layers = new Collection([...basemaps, ...operationalLayers]);
 
 // define map
 const map = new WonderMap('map', layers);
 
 // define widgets and add them to the widget-bar)
-const widgets = [new ToC()];
+const widgets = [new ToC(map.getLayers())];
 new WidgetBar(map, widgets);
