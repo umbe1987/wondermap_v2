266d3432c95cc36c1186e7d29157f51c181357fe
Umberto Minora <umberto.minora@ext.ec.europa.eu>
2020-07-08 17:19:24 +0200

    replaced ImageLayer and ImageWMS with tiled version to speed up application

--- src/layers/OperationalLayer.ts
+++ src/layers/OperationalLayer.ts
@@ -1,20 +1,19 @@
-import ImageLayer from 'ol/layer/Image';
-import ImageWMS from 'ol/source/ImageWMS';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
 
 export class OperationalLayer{
-    private operationalLayer: ImageLayer;
+    private operationalLayer: TileLayer;
     id: String;
     
     constructor(url: string, name: string, id: string) {
-        const source = new ImageWMS({
-            ratio: 1,
+        const source = new TileWMS({
             params: {'LAYERS': name},
             url: url,
         });
 
         this.id = id;
 
-        this.operationalLayer = new ImageLayer({
+        this.operationalLayer = new TileLayer({
             source: source,
         })
     }
