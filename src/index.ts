c90ee210e5815a1b414b1135334b41cfd68735ff
Umberto Minora <umberto.minora@ext.ec.europa.eu>
2020-07-15 11:39:45 +0200

    use of LayerGroup instead of Collection

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import 'ol/ol.css';
 import '../style/wondermap.css'
 import { WonderMap } from './Map';
 import { BasemapLayer } from './layers/BasemapLayer';
-import Collection from 'ol/Collection';
+import LayerGroup from 'ol/layer/Group';
 import { WidgetBar } from './widgets/widgetBar';
 import { ToC } from './widgets/toc/toc';
 
 // define basemaps
-const basemaps = new Collection([new BasemapLayer()]);
+const basemaps = new LayerGroup({
+    layers: [
+        new BasemapLayer()
+    ]});
 
 // define map and add basemaps to it
 const map = new WonderMap('map', basemaps);
 
 // define WMS URLs
-const layerURLs = ["https://www.wondermap.it/cgi-bin/qgis_mapserv.fcgi?map=/home/umberto/qgis/projects/Demo_sci_WMS/demo_sci.qgs&"];
+const layerURLs = ["https://www.wondermap.it/cgi-bin/qgis_mapserv.fcgi?map=/home/umberto/qgis/projects/Demo_sci_WMS/demo_sci.qgs&",
+"http://servizigis.regione.emilia-romagna.it/wms/areeprotette_natura2000?",
+"https://www.gebco.net/data_and_products/gebco_web_services/2019/mapserv?"];
 
 // define widgets and add them to the widget-bar
 const widgets = [
